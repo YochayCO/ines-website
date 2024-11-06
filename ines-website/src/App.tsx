fbb8746bba151dc28f7278113f632e9279e4a12f
yochayco <yochai.cohenor@gmail.com>
2024-11-06 11:51:34 +0200

    Add ugly Fullscreen toggle

--- ines-website/src/App.tsx
+++ ines-website/src/App.tsx
@@ -2,12 +2,27 @@ import Plotter from './components/Plotter/Plotter'
 import './App.css'
 
 function App() {
+  const toggleFullscreen = () => {
+    if (!document.fullscreenEnabled) {
+      console.log('fullscreen option not enabled')
+      return
+    }
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen()
+    } else {
+      document.body.requestFullscreen()
+    }
+  }
 
   return (
-    <>
+    <div className='root'>
       <h1>INES Data Sandbox</h1>
-      <Plotter />
-    </>
+        <Plotter />
+      <div className='fullscreen-button' onClick={toggleFullscreen}>
+        Toggle Fullscreen
+      </div>
+    </div>
   )
 }
 
