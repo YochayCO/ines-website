2502b5d8d817f9b82d3cca6a9bf99ce3c04a54e0
yochayco <yochai.cohenor@gmail.com>
2025-08-04 20:17:24 +0300

    Add feature flag for multi-graph view

--- ines-website/src/components/Plotter/Plotter.tsx
+++ ines-website/src/components/Plotter/Plotter.tsx
@@ -7,19 +7,20 @@ import './Plotter.css'
 
 function Plotter() {
   const [view, setView] = useState<'single' | 'multi'>('single');
+  const isProduction = window.location.href.startsWith('https://socsci4.tau.ac.il/playground');
 
   return (
     <Box>
       <Box className="plotter_toggle-group_container">
-        <ToggleButtonGroup
+        {!isProduction && <ToggleButtonGroup
           value={view}
           exclusive
           onChange={(_e, val) => val && setView(val)}
           aria-label="plotter view toggle"
         >
           <ToggleButton value="single">Single year graph</ToggleButton>
-          <ToggleButton value="multi">Multi-year graph</ToggleButton>
-        </ToggleButtonGroup>
+            <ToggleButton value="multi">Multi-year graph</ToggleButton>
+        </ToggleButtonGroup>}
       </Box>
       {view === 'single' ? <SingleSurveyPlotter /> : <MultiSurveyPlotter />}
     </Box>
